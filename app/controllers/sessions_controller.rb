6effcc6f14cf24df3ed097f8704c6ed9a92f033c
Dylan Huang <dylan.p.huang@gmail.com>
2016-11-22 11:47:47 -0800

    Just use raw consumers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,20 +11,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    puts "Before client creation"
-    puts "Token: "+auth_hash[:credentials][:token]
-    puts "Secret: "+auth_hash[:credentials][:secret]
-    puts "ENV["twitter_api_key"]: "+ENV["twitter_api_key"] 
-    puts "ENV["twitter_api_secret"]: "+ENV["twitter_api_secret"] 
-    puts "consumer_key: "+Rails.application.secrets.twitter_api_key
-    puts "consumer_secret: "+Rails.application.secrets.twitter_api_secret
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key = Rails.application.secrets.twitter_api_key
-      config.consumer_secret = Rails.application.secrets.twitter_api_secret
+      config.consumer_key = "VplV7u5kB7QEJXMPQwJbudQAH"
+      config.consumer_secret = "ZmCI2RCZa9xYqaXuCRmxmsAvSQQ1wlG6Y1NR9dmPxDwXXqjoYe"
       config.access_token = auth_hash[:credentials][:token]
       config.access_token_secret = auth_hash[:credentials][:secret]
     end
-    puts "After client creation"
     session[:twitter_id] = auth_hash[:uid]
     save_messages @user, client
     save_friends @user, client
