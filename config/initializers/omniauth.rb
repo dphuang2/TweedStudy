49e7392b4db8ba0c2757a102dc52b203365610c1
Dylan Huang <dylan.p.huang@gmail.com>
2016-11-22 11:31:15 -0800

    testing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
+  #provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
+  provider :twitter, "VplV7u5kB7QEJXMPQwJbudQAH", "ZmCI2RCZa9xYqaXuCRmxmsAvSQQ1wlG6Y1NR9dmPxDwXXqjoYe"
 end
